91341991589fd8837a6318dc79c671f818e152d1
mmenezes07 <124732099+mmenezes07@users.noreply.github.com>
2023-02-08 18:20:55 -0500

    added more tests; cleanup old tests
    - added mark as complete tests
    - renamed variables

--- e2e/pages/todo-page.ts
+++ e2e/pages/todo-page.ts
@@ -7,7 +7,9 @@ export class ToDoPage {
   readonly toDoListItems: Locator;
   readonly itemCount: Locator;
   readonly listItemEdit: Locator;
-  readonly listItem: string;
+  readonly allTab: Locator;
+  readonly activeTab: Locator;
+  readonly completedTab: Locator;
   
 
   constructor(page: Page) {
@@ -16,7 +18,9 @@ export class ToDoPage {
     this.toDoListItems = page.locator("//div[@class='view']/label");
     this.itemCount = page.locator("//span[@class='todo-count']/strong");
     this.listItemEdit = page.locator("//li[@class='editing']/input");
-    //this.listItem = `//ul/li/label[text()='${}']`
+    this.allTab = page.locator("//a[@href='#/']");
+    this.activeTab = page.locator("//a[@href='#/active']");
+    this.completedTab = page.locator("//a[@href='#/completed']");
   }
 
   async goto() {
@@ -57,5 +61,42 @@ export class ToDoPage {
     await (await this.getItem(itemName)).hover();
     // TODO clean this up. create a locator???
     await this.page.locator(`//ul[@class='todo-list']/li//label[text()='${itemName}']/following-sibling::button[@class='destroy']`).click();
+  }
+
+  async markAsComplete(itemName: string) {
+    await this.toggle(itemName, true);
+  }
+
+  async markAsInComplete(itemName: string) {
+    await this.toggle(itemName, false);
+  }
+
+  private async toggle(itemName: string, status: boolean) {
+    await this.page.locator(`//ul[@class='todo-list']/li//label[text()='${itemName}']/preceding-sibling::input`).setChecked(status);
+  }
+
+  async isItemChecked(itemName: string) {
+    return await this.page.locator(`//ul[@class='todo-list']/li//label[text()='${itemName}']/preceding-sibling::input`).isChecked();
+  }
+
+  async isTextStrikeThrough(itemName: string) {
+    let label = await this.getItem(itemName);
+    let textDecoration = await label.evaluate((element) =>
+      window.getComputedStyle(element).getPropertyValue('text-decoration')
+    );
+
+    return textDecoration.includes('line-through');
+  }
+
+  async selectAllTab() {
+    await this.allTab.click();
+  }
+
+  async selectActiveTab() {
+    await this.activeTab.click();
+  }
+
+  async selectCompletedTab() {
+    await this.completedTab.click();
   }
 }
--- e2e/tests/todo-page-test.spec.ts
+++ e2e/tests/todo-page-test.spec.ts
@@ -3,8 +3,7 @@ import { ToDoPage } from "../pages/todo-page";
 import { faker } from '@faker-js/faker';
 
 
-
-test.describe('Test Suite', () => {
+test.describe('Todo MVC Test Suite', () => {
     let toDoPage: ToDoPage;
     test.beforeAll(async ({ browser }) => {
         let page = await browser.newPage();
@@ -12,38 +11,76 @@ test.describe('Test Suite', () => {
         await toDoPage.goto();
     });
 
-    test('New todo appears last on list', async () => {
-        let newItem = faker.lorem.sentence();
-        let newItem2 = faker.lorem.sentence();
+    test('Newly added todo items appeara last on list', async () => {
+        // generate 2 items
+        let firstItem = faker.lorem.sentence();
+        let secondItem = faker.lorem.sentence();
         
-        await toDoPage.createNew(newItem);
+        // when I create a todo
+        await toDoPage.createNew(firstItem);
+        // then it appears last
         expect(await toDoPage.getItemCount()).toEqual(1);
-        expect(await toDoPage.getLastItem()).toEqual(newItem);
+        expect(await toDoPage.getLastItem()).toEqual(firstItem);
 
-        await toDoPage.createNew(newItem2);
+        // add another item to tests since if there is only a single item, it is always last
+        await toDoPage.createNew(secondItem);
         expect(await toDoPage.getItemCount()).toEqual(2);
-        expect(await toDoPage.getLastItem()).toEqual(newItem2);
-
+        expect(await toDoPage.getLastItem()).toEqual(secondItem);
     });
 
-    test('Modify todo item', async () => {
+    test('Edit todo item', async () => {
         let oldItem = faker.lorem.sentence();
         let updatedItem = faker.lorem.sentence();
 
+        // given I have created a todo item
         await toDoPage.createNew(oldItem);
-
+        // when I edit it
         await toDoPage.editItem(oldItem, updatedItem);
         
+        // then it gets updated
         expect(await toDoPage.isItemPresent(oldItem)).toBeFalsy();
         expect(await toDoPage.isItemPresent(updatedItem)).toBeTruthy();
     });
 
     test('Delete todo item', async () => {
-        let item = faker.lorem.sentence();
+        let toDoItem = faker.lorem.sentence();
         
-        await toDoPage.createNew(item);
-        await toDoPage.delete(item);
+        // given I have created a todo item
+        await toDoPage.createNew(toDoItem);
+        // when I delete it
+        await toDoPage.delete(toDoItem);
+        // then it is removed from my todo list
+        expect(await toDoPage.isItemPresent(toDoItem)).toBeFalsy();
+    });
+
+    test('Mark as complete', async () => {
+        let toDoItem = faker.lorem.sentence();
         
-        expect(await toDoPage.isItemPresent(item)).toBeFalsy();
+        // given I have created a new todo item
+        await toDoPage.createNew(toDoItem);
+        // when I mark it as complete
+        await toDoPage.markAsComplete(toDoItem);
+
+        // then it is marked with a green check mark
+        expect(await toDoPage.isItemChecked(toDoItem)).toBeTruthy();
+        // and it is crossed off my list
+        expect(await toDoPage.isTextStrikeThrough(toDoItem)).toBeTruthy();
+    });
+
+    test('Only active items in active tab', async () => {
+        let activeItem = faker.lorem.sentence();
+        let completedItem = faker.lorem.sentence();
+        
+        // given I have marked a todo item as complete
+        await toDoPage.createNew(activeItem);
+        await toDoPage.createNew(completedItem);
+        await toDoPage.markAsComplete(completedItem);
+
+        // when I view the active tab
+        await toDoPage.selectActiveTab();
+
+        // then active todo items are shown 
+        expect(await toDoPage.isItemPresent(activeItem)).toBeTruthy();
+        expect(await toDoPage.isItemPresent(completedItem)).toBeFalsy();
     });
 });
