875b1a06229bd9e0df23a3598fba4f67ff67480c
mmenezes07 <124732099+mmenezes07@users.noreply.github.com>
2023-02-08 14:42:26 -0500

    add more tests
    - added edit and delete tests
    - added method for them in todo page
    - refactored tests

--- e2e/pages/todo-page.ts
+++ e2e/pages/todo-page.ts
@@ -5,11 +5,18 @@ export class ToDoPage {
   readonly newToDo: Locator;
   readonly url: string = "https://todomvc.com/examples/react/#/";
   readonly toDoListItems: Locator;
+  readonly itemCount: Locator;
+  readonly listItemEdit: Locator;
+  readonly listItem: string;
+  
 
   constructor(page: Page) {
     this.page = page;
     this.newToDo = page.locator("//input[@class='new-todo']");
     this.toDoListItems = page.locator("//div[@class='view']/label");
+    this.itemCount = page.locator("//span[@class='todo-count']/strong");
+    this.listItemEdit = page.locator("//li[@class='editing']/input");
+    //this.listItem = `//ul/li/label[text()='${}']`
   }
 
   async goto() {
@@ -23,5 +30,32 @@ export class ToDoPage {
 
   async getLastItem() {
     return await this.toDoListItems.last().textContent();
+  }
+
+  async getItemCount() {
+    if (! await this.itemCount.isVisible()) return 0;
+    return parseInt(await this.itemCount.innerText());
+  }
+
+  private async getItem(itemName: string) {
+    return this.page.locator(`//ul[@class='todo-list']/li//label[text()='${itemName}']`);
+  }
+
+  async editItem(oldItemName: string, updatedItemName: string) {
+    await (await this.getItem(oldItemName)).dblclick();
+
+    await this.listItemEdit.clear();
+    await this.listItemEdit.type(updatedItemName);
+    await this.page.keyboard.press("Enter");
+  }
+
+  async isItemPresent(itemName: string) {
+    return await (await this.getItem(itemName)).isVisible();
+  }
+
+  async delete(itemName: string) {
+    await (await this.getItem(itemName)).hover();
+    // TODO clean this up. create a locator???
+    await this.page.locator(`//ul[@class='todo-list']/li//label[text()='${itemName}']/following-sibling::button[@class='destroy']`).click();
   }
 }
--- e2e/tests/todo-page-test.spec.ts
+++ e2e/tests/todo-page-test.spec.ts
@@ -2,11 +2,46 @@ import { test, expect } from "@playwright/test";
 import { ToDoPage } from "../pages/todo-page";
 
 
-test('New todo appears last on list', async ({ page }) => {
-    let toDoPage: ToDoPage = new ToDoPage(page);
-    let newItem = "test";
+test.describe('Test Suite', () => {
+    let toDoPage: ToDoPage;
+    test.beforeAll(async ({ browser }) => {
+        let page = await browser.newPage();
+        toDoPage = new ToDoPage(page);
+        await toDoPage.goto();
+    });
 
-    toDoPage.goto();
-    await toDoPage.createNew(newItem);
-    expect(await toDoPage.getLastItem()).toEqual("newItem");
+    test('New todo appears last on list', async () => {
+        let newItem = "test 1";
+        let newItem2 = "test 2";
+        
+        await toDoPage.createNew(newItem);
+        expect(await toDoPage.getItemCount()).toEqual(1);
+        expect(await toDoPage.getLastItem()).toEqual(newItem);
+
+        await toDoPage.createNew(newItem2);
+        expect(await toDoPage.getItemCount()).toEqual(2);
+        expect(await toDoPage.getLastItem()).toEqual(newItem2);
+
+    });
+
+    test('Modify todo item', async () => {
+        let oldItem = "test 3";
+        let updatedItem = "test 4";
+        
+        await toDoPage.createNew(oldItem);
+
+        await toDoPage.editItem(oldItem, updatedItem);
+        
+        expect(await toDoPage.isItemPresent(oldItem)).toBeFalsy();
+        expect(await toDoPage.isItemPresent(updatedItem)).toBeTruthy();
+    });
+
+    test('Delete todo item', async () => {
+        let item = "test 5";
+        
+        await toDoPage.createNew(item);
+        await toDoPage.delete(item);
+        
+        expect(await toDoPage.isItemPresent(item)).toBeFalsy();
+    });
 });
